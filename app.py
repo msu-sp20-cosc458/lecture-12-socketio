c733f892e1c701371bc047b9b1c7b1bdf6c93d4c
Phil B <philanaaurelia@gmail.com>
2019-09-13 09:00:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-import os
-import flask
+import os, flask, flask_socketio  
 
 app = flask.Flask(__name__)
+socketio = flask_socketio.SocketIO(app) 
 
 @app.route('/')
 def hello():
     return flask.render_template('index.html')
 
-app.run(
+@socketio.on('connect') 
+def on_connect():
+    print 'Someone connected!'
+    
+socketio.run(
+    app,
     host=os.getenv('IP', '0.0.0.0'),
     port=int(os.getenv('PORT', 8080)),
     debug=True
